824c136e498504bcaf65d96f30b431754f3096e6
Shiba Koenaite <koenaite8@gmail.com>
2026-03-19 09:54:00 +0200

    fixing imports

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import "tsconfig-paths/register";
 import { INestApplication, Logger, ValidationPipe, VersioningType } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
