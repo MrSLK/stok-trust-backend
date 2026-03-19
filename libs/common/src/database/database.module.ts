dfa6de0d5d4dffccb00c6c79a5e7132087e1164b
Shiba Koenaite <koenaite8@gmail.com>
2026-03-19 11:11:43 +0200

    fix import

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,13 +1,13 @@
+import { TypedConfigService } from "./../config/typed-config.service";
+import { TypedConfigModule } from "./../config/typed-config.module";
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
-import { TypedConfigModule } from "@common/config/typed-config.module";
-import { TypedConfigService } from "@common/config/typed-config.service";
 
 @Module({
   imports: [
     TypedConfigModule,
     MongooseModule.forRootAsync({
-      imports: [TypedConfigModule], // 👈 Add this too, ensures proper DI context
+      imports: [TypedConfigModule],
       useFactory: (configService: TypedConfigService) => {
         const MONGODB_URI = configService.get("mongodb.database.MONGODB_URI");
         return {
